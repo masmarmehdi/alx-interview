a1d786f35ee4b8e863a1cdfe44ecb5a2b85c595d
Mehdi Masmar <masmar507@gmail.com>
2023-12-01 10:30:53 +0100

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""N Queens Challenge"""
+"""The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem."""
 
 import sys
 
